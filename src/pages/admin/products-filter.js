a0408aa1614c2877aafd84f356ead30c7f012487
krisnadwipayana <ikadekkrisnadwipayana@gmail.com>
2023-02-18 01:41:38 +0800

    done adding filter to page

--- src/pages/admin/products-filter.js
+++ src/pages/admin/products-filter.js
@@ -6,26 +6,94 @@ import { ArrowDownIcon, ArrowUpIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
+  HStack,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  RangeSliderTrack,
   Select,
   SimpleGrid,
   Skeleton,
+  Stack,
   Table,
   TableCaption,
   TableContainer,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
   Tr,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { GetAllProduct, GetSelectProduct } from "../api/productApi";
+
+function getAllValueNotDuplicate(data = [], key) {
+  let temp = [];
+  data?.map((item) => {
+    if (temp.includes(item[key]) === false) {
+      temp.push(item[key]);
+    }
+  });
+  return temp;
+}
+
+function sortData(a, b, sortActive, sortType) {
+  if (typeof a[sortActive] === "number") {
+    if (sortType === "asc") {
+      return a[sortActive] - b[sortActive];
+    } else {
+      return b[sortActive] - a[sortActive];
+    }
+  } else {
+    var textA = a[sortActive]?.toLowerCase(),
+      textB = b[sortActive]?.toLowerCase();
+    if (sortType === "asc") {
+      if (textA < textB) {
+        return -1;
+      }
+      if (textA > textB) {
+        return 1;
+      }
+    } else {
+      if (textA > textB) {
+        return -1;
+      }
+      if (textA < textB) {
+        return 1;
+      }
+    }
+  }
+}
+
+function filterData(item, filterData) {
+  if (filterData.brand) {
+    if (item["brand"] !== filterData.brand) {
+      return false;
+    }
+  }
+  if (filterData.category) {
+    if (item["category"] !== filterData.category) {
+      return false;
+    }
+  }
+  if (filterData.priceStart) {
+    const price = item["price"];
+    if (price <= filterData.priceStart) {
+      return false;
+    }
+    if (price >= filterData.priceEnd) {
+      return false;
+    }
+  }
+  return item;
+}
 
 export default function ProductsFilter() {
   const limit = 5;
@@ -38,10 +106,19 @@ export default function ProductsFilter() {
   const [search, setSearch] = useState(1);
   const [graphData, setGraphData] = useState({});
   const [openChart, setOpenChart] = useState(false);
+
   const [clicked, setClicked] = useState(0);
   const [sortType, setSortType] = useState("");
+  const [sortActive, setSortActive] = useState("");
 
-  const [filterActive, setFilterActive] = useState("");
+  const [filterActive, setFilterActive] = useState({
+    brand: "",
+    priceStart: 0,
+    priceEnd: 0,
+    category: "",
+  });
+  const [allCategory, setAllCategory] = useState([]);
+  const [allBrand, setAllBrand] = useState([]);
 
   const labels = Object.keys(graphData);
 
@@ -65,13 +142,17 @@ export default function ProductsFilter() {
 
   useEffect(() => {
     const savedSearch = localStorage.getItem("searchQuery");
+    const savedSort = localStorage.getItem("sortQuery");
     const savedFilter = localStorage.getItem("filterQuery");
     if (savedSearch) {
       setKeyword(savedSearch);
     }
-    if (savedFilter) {
-      setFilterActive(savedFilter);
+    if (savedSort) {
+      setSortActive(savedSort);
       setClicked(1);
+    }
+    if (savedFilter) {
+      setFilterActive(JSON.parse(savedFilter));
     }
   }, []);
 
@@ -81,8 +162,13 @@ export default function ProductsFilter() {
     const limit = 100;
     GetAllProduct({ keyword, limit, skip })
       .then((res) => {
-        setDataProduct(res.data.products);
+        const { products } = res.data;
+        setDataProduct(products);
         setTotalData(res.data.total);
+        const category = getAllValueNotDuplicate(products, "category");
+        const brand = getAllValueNotDuplicate(products, "brand");
+        setAllBrand(brand);
+        setAllCategory(category);
       })
       .catch((err) => console.log(err))
       .finally(() => setIsLoading(false));
@@ -103,6 +189,7 @@ export default function ProductsFilter() {
     });
     setGraphData(temp);
   }, []);
+  useEffect(() => {}, [filterActive]);
 
   const handlePrevPage = () => setPage(page - 1);
   const handleNextPage = () => setPage(page + 1);
@@ -117,7 +204,7 @@ export default function ProductsFilter() {
     setOpenChart(false);
   };
   const HandleChangeFilter = (value) => {
-    if (filterActive === value) {
+    if (sortActive === value) {
       setClicked(clicked + 1);
       if (clicked % 2 === 0) {
         setSortType("asc");
@@ -125,42 +212,90 @@ export default function ProductsFilter() {
         setSortType("desc");
       }
     } else {
-      localStorage.setItem("filterQuery", value);
-      setFilterActive(value);
+      localStorage.setItem("sortQuery", value);
+      setSortActive(value);
       setClicked(1);
       setSortType("asc");
     }
   };
+  const handleChangeValueFilter = (e) => {
+    const { name, value } = e.target;
+    setFilterActive({ ...filterActive, [name]: value });
+    localStorage.setItem(
+      "filterQuery",
+      JSON.stringify({ ...filterActive, [name]: value })
+    );
+  };
 
-  function filteringData(a, b) {
-    if (typeof a[filterActive] === "number") {
-      if (sortType === "asc") {
-        return a[filterActive] - b[filterActive];
-      } else {
-        return b[filterActive] - a[filterActive];
-      }
-    } else {
-      var textA = a[filterActive]?.toLowerCase(),
-        textB = b[filterActive]?.toLowerCase();
-      if (sortType === "asc") {
-        if (textA < textB) {
-          return -1;
-        }
-        if (textA > textB) {
-          return 1;
-        }
-      } else {
-        if (textA > textB) {
-          return -1;
-        }
-        if (textA < textB) {
-          return 1;
-        }
-      }
-    }
-  }
+  // console.log(filterActive.brand);
+
   return (
     <Box p="5">
+      <Box pb="5">
+        <Text fontSize="2xl" fontWeight={700}>
+          Filter{" "}
+        </Text>
+        <HStack pb="2">
+          <Text>Brand: </Text>
+          <Select
+            name="brand"
+            onChange={handleChangeValueFilter}
+            placeholder="Brand"
+            value={filterActive.brand}
+          >
+            {allBrand?.map((item, key) => (
+              <option key={key} value={item}>
+                {item}
+              </option>
+            ))}
+          </Select>
+          <Text>Category: </Text>
+          <Select
+            name="category"
+            value={filterActive.category}
+            onChange={handleChangeValueFilter}
+            placeholder="Category"
+          >
+            {allCategory?.map((item, key) => (
+              <option key={key} value={item}>
+                {item}
+              </option>
+            ))}
+          </Select>
+        </HStack>
+
+        <HStack pb="2">
+          <Text w="32" wordBreak="break-word">
+            Price Range
+          </Text>
+
+          <Text mt="10">{filterActive.priceStart}</Text>
+          <RangeSlider
+            w="50%"
+            aria-label={["min", "max"]}
+            defaultValue={[0, 2000]}
+            min={0}
+            max={2000}
+            onChangeEnd={(val) => {
+              const temp = {
+                ...filterActive,
+                priceStart: val[0],
+                priceEnd: val[1],
+              };
+              setFilterActive(temp);
+              localStorage.setItem("filterQuery", JSON.stringify(temp));
+            }}
+          >
+            <RangeSliderTrack>
+              <RangeSliderFilledTrack />
+            </RangeSliderTrack>
+            <RangeSliderThumb index={0}></RangeSliderThumb>
+            <RangeSliderThumb index={1}></RangeSliderThumb>
+          </RangeSlider>
+          <Text textAlign="end">{filterActive.priceEnd}</Text>
+        </HStack>
+      </Box>
+
       <SimpleGrid columns={[1, 2]}>
         <Box>
           <Button colorScheme="purple" onClick={() => setOpenChart(true)}>
@@ -194,7 +329,7 @@ export default function ProductsFilter() {
                       onClick={() => HandleChangeFilter(item.value)}
                     >
                       {item.name}
-                      {item.value === filterActive ? (
+                      {item.value === sortActive ? (
                         sortType === "asc" ? (
                           <ArrowUpIcon />
                         ) : (
@@ -211,7 +346,8 @@ export default function ProductsFilter() {
 
             <Tbody>
               {dataProduct
-                ?.sort((a, b) => filteringData(a, b))
+                ?.filter((item) => filterData(item, filterActive))
+                .sort((a, b) => sortData(a, b, sortActive, sortType))
                 .slice(limit * (page - 1), limit * page)
                 .map((item, key) => (
                   <Tr key={key}>
